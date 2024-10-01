97d40ed75c818d5c4dbb69528b905ad0fd585b37
Salehahmedeus <97753281+Salehahmedeus@users.noreply.github.com>
2024-10-01 10:55:18 +0200

    firebase added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:gameslati/helpers/consts.dart';
 import 'package:gameslati/providers/darkmode_provider.dart';
